b15e83b682a0008016e2b61ac1a5a77a4e0fb371
keheng.chen-w6591 <keheng.chen@oatservice.com>
2025-09-17 17:30:57 +0800

    按钮功能实现

--- src/client/components/button/index.tsx
+++ src/client/components/button/index.tsx
@@ -1,16 +1,16 @@
-import { ReactNode } from 'react';
+import { ReactNode, MouseEvent, HTMLButtonElement, useRef } from 'react';
 
 import './index.scss';
 
-type ButtonType = 'default' | 'dashed' | 'text' | 'link';
-type ButtonTheme = 'default' | 'primary' | 'danger' | 'success' | 'warning' | 'info';
+type ButtonType = 'default' | 'dashed' | 'fill' | 'text' | 'link';
+type ButtonTheme = 'default' | 'primary' | 'dark' | 'danger' | 'success' | 'warning' | 'info';
 
 interface ButtonProps {
   type?: ButtonType;
   theme?: ButtonTheme;
   size?: 'normal' | 'small' | 'large' | 'mini';
   children?: ReactNode;
-  onClick?: () => void;
+  onClick?: (e: MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export function Button(props: ButtonProps) {
@@ -22,16 +22,56 @@ export function Button(props: ButtonProps) {
     onClick
   } = props;
 
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  const btnOnclick = (e: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    onClick?.(e);
+
+    if (type == 'link') {
+      return;
+    }
+
+    const btn = btnRef.current as HTMLButtonElement;
+    if (!btn) return;
+
+    // 滚动的距离 + 距离视口边界的距离 = 按钮左上角距离父标签左上角的坐标
+    const rect = btn.getBoundingClientRect();
+    const left = rect.left + window.pageXOffset;
+    const top = rect.top + window.pageYOffset;
+
+    const width = btn.clientWidth;
+    const height = btn.clientHeight;
+    const max = Math.max(width, height);
+
+    // 点击位置的坐标 - 钮左上角距离父标签左上角的距离 - ripple自身宽高的一半 = 合适的偏移坐标
+    let ripple_x = e.pageX - left - max / 2;  
+    let ripple_y = e.pageY - top - max / 2;
+
+    const rippleDiv = document.createElement('div');
+    rippleDiv.className = 'btn-ripple';
+    rippleDiv.style.width = max + 'px';
+    rippleDiv.style.height = max + 'px';
+    rippleDiv.style.left = ripple_x + 'px';
+    rippleDiv.style.top = ripple_y + 'px';
+    btn.appendChild(rippleDiv);
+
+    let timer = window.setTimeout(() => {
+      rippleDiv.remove();
+      window.clearTimeout(timer);
+    }, 3000);
+  };
+
   return (
     <button 
-      className={`predoc-button ${
-      type == 'default' ? '' : ('predoc-button-type-' + type)
-      } ${
-        theme == 'default' ? '' : ('predoc-button-theme-' + theme)
-        } ${
-          size == 'normal' ? '' : ('predoc-button-size-' + size)
+      className={`predoc-button${
+      type == 'default' ? '' : (' predoc-button-type-' + type)
+      }${
+        theme == 'default' ? '' : (' predoc-button-theme-' + theme)
+        }${
+          size == 'normal' ? '' : (' predoc-button-size-' + size)
           }`}
-      onClick={onClick}
+      onClick={btnOnclick}
+      ref={btnRef}
     >
       <span>{ children }</span>
     </button>
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -4,21 +4,26 @@ import { fn } from 'storybook/test';
 
 // import { Button } from './Button';
 import { Button } from '../client/components/button';
+import '../client/styles/vars.css';
+import '../client/styles/base.css';
+import '../client/styles/themes.css';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Predoc Inner Components/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered'
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { 
+  argTypes: {
+    type: {
+      description: '按钮类型',
+      table: {
+        defaultValue: { summary: 'default' },
+      },
+    },
+  },
+  args: {
     onClick: fn(),
     type: 'default',
     theme: 'default',
@@ -31,53 +36,286 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'Button',
-    theme: 'default',
-    size: 'normal',
+    children: 'Default Button',
     type: 'default',
   },
 };
 
-export const Default2: Story = {
+export const Dashed: Story = {
   args: {
-    children: 'Button',
-    theme: 'primary',
-    size: 'normal',
-    type: 'default',
+    children: 'Dashed Button',
+    type: 'dashed',
+  },
+  name: 'dashed'
+};
+
+export const Fill: Story = {
+  args: {
+    children: 'Fill Button',
+    type: 'fill',
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-// export const Primary: Story = {
+export const Text: Story = {
+  args: {
+    children: 'Text Button',
+    type: 'text',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    children: 'Link Button',
+    type: 'link',
+  },
+};
+
+// // 
+// export const DefaultPrimary: Story = {
 //   args: {
-//     primary: true,
-//     label: 'Button'
-//   }
+//     children: 'Default Primary',
+//     theme: 'primary',
+//     size: 'normal',
+//     type: 'default',
+//   },
 // };
 
-// export const Secondary: Story = {
+// export const DefaultDark: Story = {
 //   args: {
-//     label: 'Button'
-//   }
+//     children: 'Default Dark',
+//     theme: 'dark',
+//     type: 'default',
+//   },
 // };
 
-// export const Large: Story = {
+// export const DefaultDanger: Story = {
 //   args: {
-//     size: 'large',
-//     label: 'Button'
-//   }
+//     children: 'Default Danger',
+//     theme: 'danger',
+//     type: 'default',
+//   },
 // };
 
-// export const Small: Story = {
+// export const DefaultSuccess: Story = {
 //   args: {
-//     size: 'small',
-//     label: 'Button'
-//   }
+//     children: 'Default Success',
+//     theme: 'success',
+//     type: 'default',
+//   },
 // };
 
-// export const Predoc: Story = {
+// export const DefaultWarning: Story = {
 //   args: {
-//     primary: false,
-//     label: 'Button'
-//   }
+//     children: 'Default Warning',
+//     theme: 'warning',
+//     type: 'default',
+//   },
+// };
+
+// export const DefaultInfo: Story = {
+//   args: {
+//     children: 'Default Info',
+//     theme: 'info',
+//     type: 'default',
+//   },
+// };
+
+// // 
+// export const DashedPrimary: Story = {
+//   args: {
+//     children: 'Dashed Primary',
+//     theme: 'primary',
+//     size: 'normal',
+//     type: 'dashed',
+//   },
+// };
+
+// export const DashedDark: Story = {
+//   args: {
+//     children: 'Dashed Dark',
+//     theme: 'dark',
+//     type: 'dashed',
+//   },
+// };
+
+// export const DashedDanger: Story = {
+//   args: {
+//     children: 'Dashed Danger',
+//     theme: 'danger',
+//     type: 'dashed',
+//   },
+// };
+
+// export const DashedSuccess: Story = {
+//   args: {
+//     children: 'Dashed Success',
+//     theme: 'success',
+//     type: 'dashed',
+//   },
+// };
+
+// export const DashedWarning: Story = {
+//   args: {
+//     children: 'Dashed Warning',
+//     theme: 'warning',
+//     type: 'dashed',
+//   },
+// };
+
+// export const DashedInfo: Story = {
+//   args: {
+//     children: 'Dashed Info',
+//     theme: 'info',
+//     type: 'dashed',
+//   },
+// };
+
+// // 
+// export const FillPrimary: Story = {
+//   args: {
+//     children: 'Fill Primary',
+//     theme: 'primary',
+//     size: 'normal',
+//     type: 'fill',
+//   },
+// };
+
+// export const FillDark: Story = {
+//   args: {
+//     children: 'Fill Dark',
+//     theme: 'dark',
+//     type: 'fill',
+//   },
+// };
+
+// export const FillDanger: Story = {
+//   args: {
+//     children: 'Fill Danger',
+//     theme: 'danger',
+//     type: 'fill',
+//   },
+// };
+
+// export const FillSuccess: Story = {
+//   args: {
+//     children: 'Fill Success',
+//     theme: 'success',
+//     type: 'fill',
+//   },
+// };
+
+// export const FillWarning: Story = {
+//   args: {
+//     children: 'Fill Warning',
+//     theme: 'warning',
+//     type: 'fill',
+//   },
+// };
+
+// export const FillInfo: Story = {
+//   args: {
+//     children: 'Fill Info',
+//     theme: 'info',
+//     type: 'fill',
+//   },
+// };
+
+// //
+// export const TextPrimary: Story = {
+//   args: {
+//     children: 'Text Primary',
+//     theme: 'primary',
+//     size: 'normal',
+//     type: 'text',
+//   },
+// };
+
+// export const TextDark: Story = {
+//   args: {
+//     children: 'Text Dark',
+//     theme: 'dark',
+//     type: 'text',
+//   },
+// };
+
+// export const TextDanger: Story = {
+//   args: {
+//     children: 'Text Danger',
+//     theme: 'danger',
+//     type: 'text',
+//   },
+// };
+
+// export const TextSuccess: Story = {
+//   args: {
+//     children: 'Text Success',
+//     theme: 'success',
+//     type: 'text',
+//   },
+// };
+
+// export const TextWarning: Story = {
+//   args: {
+//     children: 'Text Warning',
+//     theme: 'warning',
+//     type: 'text',
+//   },
+// };
+
+// export const TextInfo: Story = {
+//   args: {
+//     children: 'Text Info',
+//     theme: 'info',
+//     type: 'text',
+//   },
+// };
+
+// // 
+// export const LinkPrimary: Story = {
+//   args: {
+//     children: 'Link Primary',
+//     theme: 'primary',
+//     size: 'normal',
+//     type: 'link',
+//   },
+// };
+
+// export const LinkDark: Story = {
+//   args: {
+//     children: 'Link Dark',
+//     theme: 'dark',
+//     type: 'link',
+//   },
+// };
+
+// export const LinkDanger: Story = {
+//   args: {
+//     children: 'Link Danger',
+//     theme: 'danger',
+//     type: 'link',
+//   },
+// };
+
+// export const LinkSuccess: Story = {
+//   args: {
+//     children: 'Link Success',
+//     theme: 'success',
+//     type: 'link',
+//   },
+// };
+
+// export const LinkWarning: Story = {
+//   args: {
+//     children: 'Link Warning',
+//     theme: 'warning',
+//     type: 'link',
+//   },
+// };
+
+// export const LinkInfo: Story = {
+//   args: {
+//     children: 'Link Info',
+//     theme: 'info',
+//     type: 'link',
+//   },
 // };
