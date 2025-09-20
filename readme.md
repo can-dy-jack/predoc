d5fb88fcd106de5288172237e528e4b077215cc3
can-dy-jack <jjack_chen3@163.com>
2025-09-20 14:02:02 +0800

    保存pr修改

--- readme.md
+++ readme.md
@@ -2,13 +2,28 @@
 > 目标：可拓展的、灵活的静态文档生成器
 
 ## todo
-- 打包之后，样式加载有问题
-- 打包体积过大 - 分析、减少三方包的打包 📦
-
-- create-predoc
+- helmet
+- [ ]样式统一
+  - [x] css变量
+  - 主题支持：白色、黑暗（监听系统自动切换 or 手动设置跟随系统 - 都通过 .dark 的 class 实现）
+- 打包问题
+  - 打包之后，样式加载有问题。特别是图标相关的，或者unocss
+  - 打包体积过大 - 分析、减少三方包的打包 📦
+  - 打包有时候会失败，疑似build顺序问题。暂时在windows下出现该问题
+- 组件
+  - 基础组件：按钮、图标、下拉菜单、popup、tab、table、form相关
+  - react motion
+  - 首页：仿苹果官网动画效果 - 封装成组件、或者已经有组件
+    - https://juejin.cn/post/7078801234097864741
+    - https://juejin.cn/post/7521160007854866467
+    - https://juejin.cn/post/7178698886721044539
+- 代码结构升级
+  - create-predoc
+  - 说明文档
 
 ## run
 ```bash
+pnpm install
 pnpm build
 
 pnpm link
--- src/client/components/button/index.tsx
+++ src/client/components/button/index.tsx
@@ -1,39 +1,79 @@
-import React from 'react';
-import { Link } from '../link';
-import styles from './index.module.scss';
+import { ReactNode, MouseEvent, HTMLButtonElement, useRef } from 'react';
+
+import './index.scss';
+
+type ButtonType = 'default' | 'dashed' | 'fill' | 'text' | 'link';
+type ButtonTheme = 'default' | 'primary' | 'dark' | 'danger' | 'success' | 'warning' | 'info';
 
 interface ButtonProps {
-  type?: string;
-  size?: 'medium' | 'big';
-  theme?: 'brand' | 'alt';
-  text: string;
-  href?: string;
-  external?: boolean;
-  className?: string;
+  type?: ButtonType;
+  theme?: ButtonTheme;
+  size?: 'normal' | 'small' | 'large' | 'mini';
+  children?: ReactNode;
+  onClick?: (e: MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export function Button(props: ButtonProps) {
   const {
-    theme = 'brand',
-    size = 'big',
-    href = '/',
-    external = false,
-    className = ''
+    type = 'default',
+    theme = 'primary',
+    size = 'normal',
+    children,
+    onClick
   } = props;
-  let type: string | typeof Link | null = null;
 
-  if (props.type === 'button') {
-    type = 'button';
-  } else if (props.type === 'a') {
-    type = external ? 'a' : Link;
-  }
+  const btnRef = useRef<HTMLButtonElement>(null);
 
-  return React.createElement(
-    type ?? 'a',
-    {
-      className: `${styles.button} ${styles[theme]} ${styles[size]} ${className}`,
-      href
-    },
-    props.text
+  const btnOnclick = (e: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    onClick?.(e);
+
+    if (type == 'link') {
+      return;
+    }
+
+    const btn = btnRef.current as HTMLButtonElement;
+    if (!btn) return;
+
+    // 滚动的距离 + 距离视口边界的距离 = 按钮左上角距离父标签左上角的坐标
+    const rect = btn.getBoundingClientRect();
+    const left = rect.left + window.pageXOffset;
+    const top = rect.top + window.pageYOffset;
+
+    const width = btn.clientWidth;
+    const height = btn.clientHeight;
+    const max = Math.max(width, height);
+
+    // 点击位置的坐标 - 钮左上角距离父标签左上角的距离 - ripple自身宽高的一半 = 合适的偏移坐标
+    let ripple_x = e.pageX - left - max / 2;  
+    let ripple_y = e.pageY - top - max / 2;
+
+    const rippleDiv = document.createElement('div');
+    rippleDiv.className = 'btn-ripple';
+    rippleDiv.style.width = max + 'px';
+    rippleDiv.style.height = max + 'px';
+    rippleDiv.style.left = ripple_x + 'px';
+    rippleDiv.style.top = ripple_y + 'px';
+    btn.appendChild(rippleDiv);
+
+    let timer = window.setTimeout(() => {
+      rippleDiv.remove();
+      window.clearTimeout(timer);
+    }, 3000);
+  };
+
+  return (
+    <button 
+      className={`predoc-button${
+      type == 'default' ? '' : (' predoc-button-type-' + type)
+      }${
+        theme == 'default' ? '' : (' predoc-button-theme-' + theme)
+        }${
+          size == 'normal' ? '' : (' predoc-button-size-' + size)
+          }`}
+      onClick={btnOnclick}
+      ref={btnRef}
+    >
+      <span>{ children }</span>
+    </button>
   );
 }
